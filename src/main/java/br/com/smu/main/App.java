7e5e0dfe1d6cb5d48e4317321587f70e0e334076
Thiago Yudji <thiyudji@gmail.com>
2020-03-30 15:03:16 -0300

    Alterando projeto de acordo com solicitacoes do pedro

--- src/main/java/br/com/smu/main/App.java
+++ src/main/java/br/com/smu/main/App.java
@@ -1,78 +1,47 @@
 package br.com.smu.main;
 import static spark.Spark.after;
 import static spark.Spark.before;
+import static spark.Spark.delete;
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.put;
 
 import java.util.Date;
 
-import org.eclipse.jetty.http.HttpStatus;
-import org.eclipse.jetty.http.MimeTypes;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.gson.Gson;
-
-import br.com.smu.entity.Usuario;
-import br.com.smu.service.UsuarioService;
 import br.com.smu.transformer.JsonTransformer;
-import spark.utils.StringUtils;
+import br.com.smu.validator.UsuarioValidator;
+
 
 public class App {
-	public static void main(String[] args) {		
-		Logger logger = LoggerFactory.getLogger(App.class);
-		UsuarioService usuarioService = new UsuarioService();
+	public static void main(String[] args) {
 
-		
-		//interceptors
 		before((req, res) -> {
-			logger.info(String.format("Requisicao recebida as %s", new Date()));
+			UsuarioValidator validator = new UsuarioValidator();
+			validator.isValido(req);
+			System.out.println("Requisicao iniciada as %s" + new Date());
 		});
 
-		
 		after((req, res) -> {
-			logger.info(String.format("Requisicao encerrada as %s", new Date()));
+			System.out.println("Requisicao recebida as %s" + new Date());
 		});
 
-		//api
-		get("/user/:id", MimeTypes.Type.APPLICATION_JSON.asString(), (req, res) -> {			
-			Long idUsuario = Long.valueOf(!StringUtils.isEmpty(req.params("id")) ? req.params("id") : "0");
-			Usuario usuario = usuarioService.buscarPorId(idUsuario);
-			if (usuario == null) {
-				res.status(HttpStatus.NO_CONTENT_204);
-			}
-			return usuario;
+		get("/user/:id", (req, res) -> {			
+			return "Usuario encontrado";
 		}, new JsonTransformer());
 		
 		
-		get("/", MimeTypes.Type.APPLICATION_JSON.asString(), (req, res) -> {
-			return usuarioService.listarTodos();
-		}, new JsonTransformer());
-		
-		
-		post("/user", MimeTypes.Type.APPLICATION_JSON.asString(), (req, res) -> {
-			Usuario usuario = new Gson().fromJson(req.body(), Usuario.class);
-			usuarioService.salvar(usuario);
-			
-			if(usuario.getId() == null || usuario.getId() < 1l) {
-				res.status(HttpStatus.BAD_REQUEST_400);
-			}
-			
-			return usuario;			
+		post("/user", (req, res) -> {
+			return "Usuario criado";			
 		}, new JsonTransformer());
 		
 
-		put("/user/:id", MimeTypes.Type.APPLICATION_JSON.asString(), (req, res) -> {
-			Long idUsuario = Long.valueOf(!StringUtils.isEmpty(req.params("id")) ? req.params("id") : "0");
-			Usuario usuario = new Gson().fromJson(req.body(), Usuario.class);
-			usuarioService.atualizar(usuario, idUsuario);
-			
-			if(usuario.getId() == null || usuario.getId() < 1l) {
-				res.status(HttpStatus.BAD_REQUEST_400);
-			}
-			
-			return usuario;			
+		put("/user/:id", (req, res) -> {
+			return "Usuario atualizado";
+		}, new JsonTransformer());
+		
+		
+		delete("/user/:id",(req, res) ->{
+			return "Usuario deletado";
 		}, new JsonTransformer());
 	}
 
